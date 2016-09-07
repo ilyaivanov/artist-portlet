0cc2877a6fcd40c0af1c540c5e267702cbf9b255
Ilya Ivanov <static.ila@gmail.com>
2016-09-07 11:22:47 +0300

    Add collapsing behaviour

---
+++ script.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+  
+    $(function () {
+        $('.artist-title').click(function(event){
+            $('.list-group').collapse('toggle');     
+        });
+    });
+    
+}());
